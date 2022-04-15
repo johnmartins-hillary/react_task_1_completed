4c4bf271a4166ebcdd6e6e5fafe18bf61da54988
johnmartins-hillary <ekwealorhillary@gmail.com>
2022-04-15 07:08:19 -0700

    installed react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import "./App.css";
 import Home from "./pages/Home/Home";
 import AuthProvider from "./context/AuthProvider";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Admin from "./pages/admin/Admin";
 
 function App() {
   return (
     <div className="App">
       <AuthProvider>
-        <Home></Home>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/admin" element={<Admin />}></Route>
+          </Routes>
+        </Router>
       </AuthProvider>
     </div>
   );
